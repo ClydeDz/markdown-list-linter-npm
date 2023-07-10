854091e5faa3990abbe99210b1627c311b340a2e
Clyde D'Souza <clyd94@gmail.com>
2023-07-11 00:54:47 +1200

    Run prettier fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,38 +1,38 @@
 module.exports = {
-    env: {
-      browser: true,
-      commonjs: true,
-      es6: true,
-      jest: true,
-      node: true,
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    jest: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-    parser: '@babel/eslint-parser',
-    parserOptions: {
-      ecmaFeatures: {
-        jsx: true,
-      },
-      ecmaVersion: 2018,
-      sourceType: 'module',
-      requireConfigFile: false,
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    requireConfigFile: false,
+  },
+  plugins: [],
+  overrides: [
+    {
+      files: ['**/*.ts?(x)'],
+      parser: '@typescript-eslint/parser',
+      plugins: ['@typescript-eslint'],
     },
-    plugins: [],
-    overrides: [
-      {
-        files: ['**/*.ts?(x)'],
-        parser: '@typescript-eslint/parser',
-        plugins: ['@typescript-eslint'],
-      },
-    ],
-    root: true,
-    rules: {
-      'no-console': 'error',
-      'multiline-comment-style': ['error', 'starred-block'],
-      'no-inline-comments': 'error',
+  ],
+  root: true,
+  rules: {
+    'no-console': 'error',
+    'multiline-comment-style': ['error', 'starred-block'],
+    'no-inline-comments': 'error',
+  },
+  settings: {
+    react: {
+      version: 'detect',
     },
-    settings: {
-      react: {
-        version: 'detect',
-      },
-    },
-  }
+  },
+}
